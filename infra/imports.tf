decece23a99616442ad538c024d8779ba6ae2f99
Jeremy <>
2025-07-17 22:01:49 -0400

    tf fix 2

--- infra/imports.tf
+++ infra/imports.tf
@@ -1,19 +1,18 @@
 # Terraform Import Configuration
 # This file defines how to import existing resources automatically
-# Currently disabled to allow Terraform to create resources cleanly
+# Enabled to import resources that already exist in Azure
 
-# NOTE: Import blocks are commented out since we're letting Terraform create all resources
-# Uncomment these if you need to import existing resources later
+# Import existing resource group
+import {
+  to = azurerm_resource_group.healthcare_rg
+  id = "/subscriptions/82ba81d8-2ee1-4c98-87ce-dc378bef4592/resourceGroups/healthcare-content-rg"
+}
 
-# import {
-#   to = azurerm_resource_group.healthcare_rg
-#   id = "/subscriptions/82ba81d8-2ee1-4c98-87ce-dc378bef4592/resourceGroups/healthcare-content-rg"
-# }
-
-# import {
-#   to = azurerm_storage_account.healthcare_storage
-#   id = "/subscriptions/82ba81d8-2ee1-4c98-87ce-dc378bef4592/resourceGroups/healthcare-content-rg/providers/Microsoft.Storage/storageAccounts/healthcarestorefv0vlbg2"
-# }
+# Import existing storage account
+import {
+  to = azurerm_storage_account.healthcare_storage
+  id = "/subscriptions/82ba81d8-2ee1-4c98-87ce-dc378bef4592/resourceGroups/healthcare-content-rg/providers/Microsoft.Storage/storageAccounts/healthcarestorefv0vlbg2"
+}
 
 # import {
 #   to = azurerm_storage_container.healthcare_container
